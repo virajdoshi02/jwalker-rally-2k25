e398cbb8a4c5ac8a271c33821d5d430a8774c151
Charles Wang <czw@seas.upenn.edu>
2024-11-03 03:28:23 -0500

    basic ui

--- Assets/Scripts/RCC.cs
+++ Assets/Scripts/RCC.cs
@@ -5,26 +5,27 @@ public class RCC : MonoBehaviour
     public TrackGenerator track;
     public GameObject Player;
 
+    [SerializeField]
+    private UIManager ui;
+
     public static RCC I { get; private set; }
 
     public CarSpawner carSpawner;
     private float distThreshold = 10.0f;
 
-    bool start = false;
-    int terribleHack = 1;
+    private bool start = false;
+    private int terribleHack = 1;
 
-    public void CheckPlayerTargetDelta()
-    {
+    public void CheckPlayerTargetDelta() {
         if (I.track.targets.Count == 0) return;
         if (!start) Player.transform.position = I.track.targets[0] + Vector3.up * 2.0f;
         start = true;
 
-        Vector3 currentTarget = I.track.targets[2];
-        Vector3 playerPos = Player.transform.position; // TO DO: replace with the actual player position
-        float distToTarget = (playerPos - currentTarget).magnitude;
-        
-        if (distToTarget < distThreshold)
-        {
+        var currentTarget = I.track.targets[2];
+        var playerPos = Player.transform.position; // TO DO: replace with the actual player position
+        var distToTarget = (playerPos - currentTarget).magnitude;
+
+        if (distToTarget < distThreshold) {
             carSpawner.CarSpawn(5);
             carSpawner.CarSpawn(7);
             carSpawner.CarSpawn(9);
@@ -32,9 +33,11 @@ public class RCC : MonoBehaviour
             track.IncrementTrack();
             I.track.targets.RemoveAt(0);
 
-            if (terribleHack == 0)
-            {
-                Debug.Log("bye son");
+            // Increment score!
+            ui.IncreaseDistance();
+
+            if (terribleHack == 0) {
+                // Debug.Log("bye son");
                 I.track.PopPlane();
             }
 
@@ -52,8 +55,11 @@ public class RCC : MonoBehaviour
         }
     }
 
-    private void Update()
-    {
+    private void Update() {
         CheckPlayerTargetDelta();
+    }
+
+    public void LoseGame() {
+        ui.PauseTimer();
     }
 }
